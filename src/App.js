d154ffa168967336dbe07eea6a1d56c5f816333e
Dey11subrata <deysubrata2311@gmail.com>
2023-11-13 16:10:07 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { ShowTasks } from "./components/ShowTasks";
 import "./App.css";
 
 function App() {
+  const [tasksList, setTasksList] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <AddTask />
-      <ShowTasks />
+      <AddTask tasksList={tasksList} setTasksList={setTasksList} />
+      <ShowTasks tasksList={tasksList} setTasksList={setTasksList} />
     </div>
   );
 }
--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -1,48 +1,25 @@
 import React from 'react'
 import "./ShowTasks.css"
 
-export const ShowTasks = () => {
-  const tasks = [
-    {
-      id: 1001,
-      name: "Task A",
-      time: new Date()
-    },
-    {
-      id: 1002,
-      name: "Task B",
-      time: new Date()
-    },
-    {
-      id: 1003,
-      name: "Task C",
-      time: new Date()
-    },
-    {
-      id: 1004,
-      name: "Task D ",
-      time: new Date()
-    },
-  ]
-  return (
+export const ShowTasks = ({tasksList, setTasksList}) => {
+    return (
     <section className='showTask'>
         <div className='head'>
             <div>
                 <span className='title'>Todo</span>
-                <span className='count'>0</span>
+                <span className='count'>{tasksList.length}</span>
             </div>
-            <button className='clearAll'>Clear All</button>
+            <button className='clearAll' onClick={()=>setTasksList([])}>Clear All</button>
         </div>
         <ul>
-          {tasks.map(task => (
-            <li key={task.id}>
-            <p>
-              <span className='name'>{task.name}</span>
-              <span></span>
-            </p>
-            <i className='bi bi-pencil-square'></i>
-            <i className='bi bi-trash'></i>
-            {/* <p>{task.time}</p> */}
+          {tasksList.map(task => (
+              <li key={task.id}>
+              <p>
+                <span className='name'>{task.name}</span>
+                <span className='time'>{task.time}</span>
+              </p>
+              <i className='bi bi-pencil-square'></i>
+              <i className='bi bi-trash'></i>
             </li>
           ))}
         </ul>
