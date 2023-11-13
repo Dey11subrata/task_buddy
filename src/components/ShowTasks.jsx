fda4b19c5a3cc0e7ad530e17fd4074e9e57ed79b
Dey11subrata <deysubrata2311@gmail.com>
2023-11-13 11:52:09 +0530

    base structure ready

--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 import "./ShowTasks.css"
 
 export const ShowTasks = () => {
+  const tasks = [
+    {
+      id: 1001,
+      name: "Task A",
+      time: new Date()
+    },
+    {
+      id: 1002,
+      name: "Task B",
+      time: new Date()
+    },
+    {
+      id: 1003,
+      name: "Task C",
+      time: new Date()
+    },
+    {
+      id: 1004,
+      name: "Task D ",
+      time: new Date()
+    },
+  ]
   return (
     <section className='showTask'>
         <div className='head'>
@@ -11,6 +33,19 @@ export const ShowTasks = () => {
             </div>
             <button className='clearAll'>Clear All</button>
         </div>
+        <ul>
+          {tasks.map(task => (
+            <li key={task.id}>
+            <p>
+              <span className='name'>{task.name}</span>
+              <span></span>
+            </p>
+            <i className='bi bi-pencil-square'></i>
+            <i className='bi bi-trash'></i>
+            {/* <p>{task.time}</p> */}
+            </li>
+          ))}
+        </ul>
     </section>
   )
 }
