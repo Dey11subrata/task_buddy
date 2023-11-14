6c222dfab92d2bc2b6af12ef6b128b3196d6ca50
Dey11subrata <deysubrata2311@gmail.com>
2023-11-14 11:28:19 +0530

    theme selection added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from "../assets/logo.png"
 import "./Header.css"
 
 export const Header = () => {
+  const [theme, setTheme]= useState('light')
+
+  useEffect(()=>{
+    document.documentElement.removeAttribute('class')
+    document.documentElement.classList.add(theme)
+  },[theme])
+
   return (
     <header >
         <div className="logo">
@@ -10,12 +17,12 @@ export const Header = () => {
             <span>Task Buddy</span>
         </div>
         <div className="themeSelector">
-            <span className='light'></span>
-            <span className='medium'></span>
-            <span className='dark'></span>
-            <span className='gradientOne'></span>
-            <span className='gradientTwo'></span>
-            <span className='gradientThree'></span>
+            <span onClick={()=>setTheme('light')} className={theme === 'light'?'light activeTheme':'light'}></span>
+            <span onClick={()=>setTheme('medium')} className={theme === 'medium'?'medium activeTheme':'medium'}></span>
+            <span onClick={()=>setTheme('dark')} className={theme === 'dark'?'dark activeTheme':'dark'}></span>
+            <span onClick={()=>setTheme('gradientOne')} className={theme === 'gradientOne'?'gradientOne activeTheme':'gradientOne'}></span>
+            <span onClick={()=>setTheme('gradientTwo')} className={theme === 'gradientTwo'?'gradientTwo activeTheme':'gradientTwo'}></span>
+            <span onClick={()=>setTheme('gradientThree')} className={theme === 'gradientThree'?'gradientThree activeTheme':'gradientThree'}></span>
         </div>
     </header>
   )
